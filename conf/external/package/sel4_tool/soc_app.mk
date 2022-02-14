c935d783bb463040b10aa3e668736a0d30481047
Tero Salminen <tero.salminen@unikie.com>
2022-02-14 14:41:43 +0200

    sel4-tool: use git repository
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I3d8106a7c089ae88da268bdc689de97509659ef9

--- conf/external/package/sel4_tool/soc_app.mk
+++ conf/external/package/sel4_tool/soc_app.mk
@@ -4,16 +4,17 @@
 #
 ################################################################################
 
-SEL4_TOOL_VERSION = 1.0
-SEL4_TOOL_SITE = $(BR2_EXTERNAL_SEL4_AMP_PATH)/package/sel4_tool/src
-SEL4_TOOL_SITE_METHOD = local
+SEL4_TOOL_VERSION = platsec_dev
+SEL4_TOOL_SITE = ssh://gerrit.ssrc-tre.fi:29418/Platsec/teeos_host
+SEL4_TOOL_SITE_METHOD = git
+SEL4_TOOL_INSTALL_TARGET = YES
 
 define SEL4_TOOL_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/sel4_tool
 endef
 
 define SEL4_TOOL_INSTALL_TARGET_CMDS
-   $(INSTALL) -D -m 0755 $(@D)/sel4-tool $(TARGET_DIR)/usr/bin
+   $(INSTALL) -D -m 0755 $(@D)/sel4_tool/sel4-tool $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
