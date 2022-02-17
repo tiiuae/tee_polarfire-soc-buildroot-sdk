783471c33d051a9af5518bb29eb5713aab77a8b7
Tero Salminen <tero.salminen@unikie.com>
2022-02-17 14:23:36 +0200

    sel4-tool: convert to cmake build
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I432603b6acedff897155b2e14193dc7b372e909d

--- conf/external/package/sel4_tool/soc_app.mk
+++ conf/external/package/sel4_tool/soc_app.mk
@@ -8,13 +8,6 @@ SEL4_TOOL_VERSION = platsec_dev
 SEL4_TOOL_SITE = ssh://gerrit.ssrc-tre.fi:29418/Platsec/teeos_host
 SEL4_TOOL_SITE_METHOD = git
 SEL4_TOOL_INSTALL_TARGET = YES
+SEL4_TOOL_GIT_SUBMODULES = YES
 
-define SEL4_TOOL_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/sel4_tool
-endef
-
-define SEL4_TOOL_INSTALL_TARGET_CMDS
-   $(INSTALL) -D -m 0755 $(@D)/sel4_tool/sel4-tool $(TARGET_DIR)/usr/bin
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
