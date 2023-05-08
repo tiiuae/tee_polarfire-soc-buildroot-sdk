f8e5efe9c4395cc3eacc3027a255d1608e51bba3
everton-dematos <everton@ssrc.tii.ae>
2023-05-08 16:01:45 +0400

    Update SEL4_TOOL_SITE
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- conf/external/package/sel4_tool/soc_app.mk
+++ conf/external/package/sel4_tool/soc_app.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SEL4_TOOL_VERSION = platsec_dev
-SEL4_TOOL_SITE = ssh://gerrit.ssrc-tre.fi:29418/Platsec/teeos_host
+SEL4_TOOL_SITE = https://github.com/tiiuae/optee_teeos_host
 SEL4_TOOL_SITE_METHOD = git
 SEL4_TOOL_INSTALL_TARGET = YES
 SEL4_TOOL_GIT_SUBMODULES = YES
